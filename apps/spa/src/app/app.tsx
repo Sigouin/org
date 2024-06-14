ed80879b70c8d92d8c8cdda4f723a73928824710
Matthew <matt@sigouin.codes>
2024-06-14 16:49:25 -0400

    Setup React Router and connected Products

--- apps/spa/src/app/app.tsx
+++ apps/spa/src/app/app.tsx
@@ -1,13 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
+import { Products } from '@org/products';
+
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
 
-import NxWelcome from './nx-welcome';
+function Home() {
+  return <h1>Home</h1>;
+}
 
 export function App() {
   return (
-    <div>
-      <NxWelcome title="spa" />
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/products" element={<Products />}></Route>
+    </Routes>
   );
 }
 
